5ec5a3a18c22ef3ac0c19733afda1ffa8b8738b6
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2023-07-01 17:31:59 +0530

    patch: Added Version to /health healthcheck route

--- routes/health.routes.ts
+++ routes/health.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import pkg from '../package.json' assert { type: 'json' }
 
 const router = Router()
 
@@ -6,6 +7,7 @@ router.use('/', (req, res) => {
     return res.status(200).json({
         status: 'OK',
         app: 'B68 API',
+        version: pkg.version,
         request_ip: req.ip,
         uptime: process.uptime(),
         hrtime: process.hrtime(),
