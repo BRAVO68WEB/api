150e40199ccbb8443f8a0dc34143e7a529fb8010
bravo68web <jbandyopadhayaya@gmail.com>
2023-09-17 22:34:49 +0530

    feat: Update cli to support hono api struct + opsec

--- helpers/auth_client.ts
+++ helpers/auth_client.ts
@@ -2,26 +2,15 @@ import KEYCLOAK_ISSUER from "../configs/keycloak";
 
 const authClient = new KEYCLOAK_ISSUER.Client({
     client_id: process.env.KEYCLOAK_CLIENT_ID,
-    // 'auth-server-url':
-    //     process.env.KEYCLOAK_AUTH_SERVER_URL +
-    //     '/realms/' +
-    //     process.env.KEYCLOAK_REALM,
-    // 'ssl-required': 'all',
-    // resource: process.env.KEYCLOAK_CLIENT_ID,
-    // credentials: {
-    //     'secret-jwt': { secret: process.env.KEYCLOAK_CLIENT_SECRET },
-    // },
-    // 'confidential-port': 0,
-    // redirect_uri: process.env.KEYCLOAK_REDIRECT_URI,
     client_secret: process.env.KEYCLOAK_CLIENT_SECRET,
-    // default_max_age: 3600000,
-    realm: "master",
-    "auth-server-url": "https://auth.b68.dev/",
+    default_max_age: 3600000,
+    realm: process.env.KEYCLOAK_REALM,
+    "auth-server-url": process.env.KEYCLOAK_AUTH_SERVER_URL,
     "ssl-required": "external",
-    resource: "b68-api",
+    resource: process.env.KEYCLOAK_CLIENT_ID,
     "verify-token-audience": true,
     credentials: {
-        secret: "vHUwpMA5Ccqea8JY7xLCDtatEump9yuD",
+        secret: process.env.KEYCLOAK_CLIENT_SECRET,
     },
     "confidential-port": 0,
     "policy-enforcer": {
