1b0e12d6eb9b93f331290e794f63fc562ff0cc4a
bravo68web <jbandyopadhayaya@gmail.com>
2023-09-17 19:36:39 +0530

    feat: Worked on `api` for ip,ping + home(jsx)

---
+++ app/main.tsx
@@ -0,0 +1,38 @@
+import { Hono } from 'hono'
+import { html } from 'hono/html'
+
+const app = new Hono()
+
+interface SiteData {
+  title: string
+  children?: any
+}
+
+const Layout = (props: SiteData) => html`<!DOCTYPE html>
+  <html>
+    <head>
+      <title>${props.title}</title>
+    </head>
+    <body>
+      ${props.children}
+    </body>
+  </html>`
+
+const Content = (props: { siteData: SiteData; name: string }) => (
+  <Layout {...props.siteData}>
+    <h1>Hello from {props.name}</h1>
+  </Layout>
+)
+
+app.get('/', (c) => {
+  const name = 'B68 API'
+  const props = {
+    name: name,
+    siteData: {
+      title: 'B68 API',
+    },
+  }
+  return c.html(<Content {...props} />)
+})
+
+export default app
