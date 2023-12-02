438cdb49547087eeb6437e43405a72e95bc019f0
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2023-12-02 13:24:58 +0530

    feat: added contact me api route

--- routes/api/index.ts
+++ routes/api/index.ts
@@ -3,11 +3,13 @@ import { Hono } from "hono";
 import ip from "./ip.routes";
 import ping from "./ping.routes";
 import upload from "./upload.routes";
+import contact from "./contact.routes";
 
 const router = new Hono();
 
 router.route("/ip", ip);
 router.route("/ping", ping);
 router.route("/upload", upload);
+router.route("/contact", contact);
 
 export default router;
---
+++ services/contactme.service.ts
@@ -0,0 +1,14 @@
+import mailer from "../helpers/mailer";
+import { IContactFormInput } from "../types";
+
+export default class IPInfo {
+    public postQueryToMail = async (contactData: IContactFormInput) => {
+        return await mailer({
+            to: "hi-api@b68.dev",
+            from: "api@b68web.xyz",
+            replyTo: contactData.email,
+            subject: "New Contact form regarding " + contactData.subject,
+            text: `Name: ${contactData.name}\nEmail: ${contactData.email}\nMessage: ${contactData.message}`,
+        });
+    };
+}
