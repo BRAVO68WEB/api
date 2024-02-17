7ef94f7a4c2948485dc872c1ee17e7e52a560098
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2024-02-17 14:15:54 +0530

    fix: osu auth changes

--- services/osu.service.ts
+++ services/osu.service.ts
@@ -4,19 +4,20 @@ const config = process.env;
 
 export default class Osu {
     constructor() {
-        auth.login_lazer(config.OSU_USERNAME, config.OSU_PASSWORD);
+        auth.login(Number(config.OSU_CLIENT_ID), config.OSU_CLIENT_SECRET, ['public']);
     }
 
     public async getOsuSelf() {
-        return await v2.user.me.details("osu");
+        // await auth.login(Number(config.OSU_CLIENT_ID), config.OSU_CLIENT_SECRET, ['public']);
+        return await v2.user.details(15_227_110, "osu");
     }
 
     public async bestScoresSelf() {
-        return await v2.user.scores.category(15_227_110, "best", {});
+        return await v2.scores.user.category(15_227_110, "best", {});
     }
 
     public async recentScoresSelf() {
-        return await v2.user.scores.category(15_227_110, "recent", {});
+        return await v2.scores.user.category(15_227_110, "recent", {});
     }
 
     public async favouriteBeatmapsSelf() {
